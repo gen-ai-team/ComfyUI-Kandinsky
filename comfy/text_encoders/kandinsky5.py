56841ba4a71323035d5c0f0e5557cea39f1ea16d
Mihail Karaev <karaevmihail@gmail.com>
2026-01-21 09:22:36 +0000

    Fix llama_template I2I

--- comfy/text_encoders/kandinsky5.py
+++ comfy/text_encoders/kandinsky5.py
@@ -24,7 +24,7 @@ class Kandinsky5TokenizerImage(Kandinsky5Tokenizer):
 class Kandinsky5TokenizerI2I(Kandinsky5Tokenizer):
     def __init__(self, embedding_directory=None, tokenizer_data={}):
         super().__init__(embedding_directory=embedding_directory, tokenizer_data=tokenizer_data)
-        self.llama_template_images = "<|im_start|>system\nYou are a promt engineer. Based on the provided source image (first image) and target image (second image), create an interesting text prompt that can be used together with the source image to create the target image:<|im_end|>\n<|im_start|>user\n{}<|im_end|><|im_start|>assistant\n<|vision_start|><|image_pad|><|vision_end|><|im_end|>"
+        self.llama_template_images = "<|im_start|>system\nYou are a promt engineer. Based on the provided source image (first image) and target image (second image), create an interesting text prompt that can be used together with the source image to create the target image:<|im_end|>\n<|im_start|>user\n{}<|vision_start|><|image_pad|><|vision_end|><|im_end|>"
 
 
 class Qwen25_7BVLIModel(sd1_clip.SDClipModel):
